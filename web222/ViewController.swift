ff097e61423392104162e88821442af4098dc5c9
Nylon <diego.gomes87@gmail.com>
2015-11-13 20:45:29 -0200

    o site q estava usando por algum motivo esta bloqueando o acesso,alterei para o meu blog

--- web222/ViewController.swift
+++ web222/ViewController.swift
@@ -10,9 +10,27 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    @IBOutlet var webview: UIWebView!
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+        let url = NSURL(string: "http://nylondev.wordpress.com")
+        
+        let task = NSURLSession.sharedSession().dataTaskWithURL(url!){
+            (data, reposnse, error) in
+            
+            if error == nil {
+                let urlContent = NSString(data: data!, encoding: NSUTF8StringEncoding)
+                print(urlContent)
+                
+                //essa linha aqui faz com que o processo de carregar a pagina se torno 20% mais rapido
+                dispatch_async(dispatch_get_main_queue()) {
+                    self.webview.loadHTMLString(urlContent! as String, baseURL: nil)
+                }
+            }
+        }
+        
+        task.resume()
     }
 
     override func didReceiveMemoryWarning() {
